2181fd2564f3d073ff964cb2225cef0d97aa4181
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-05-19 01:13:38 +0300

    Adding Pelican project

---
+++ content/pages/about.rst
@@ -0,0 +1,7 @@
+About
+#####
+
+:date: 2014-05-19
+:url: 
+:save_as: index.html
+
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jordan Jambazov'
+SITENAME = u'Jordan Jambazov'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Sofia'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Empybit', 'http://empybit.com/'),
+         )
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/jordanjambazov'),
+          ('LinkedIn', 'https://www.linkedin.com/in/jordanjambazov'),
+          ('GitHub', 'https://github.com/jordanjambazov'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'jambazov'
